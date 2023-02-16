d5989422cdb7445e71ed49e178efa75a009c52a4
Somon <somon.olimzoda1@gmail.com>
2023-02-17 03:30:17 +0900

    Update .sql

--- .sql
+++ .sql
@@ -35,11 +35,29 @@ group by "race/ethnicity"
 order by num_groups desc;
 
 
+-- # of students by race/ethnicity grouped by gender
+select gender,
+"race/ethnicity", 
+count("race/ethnicity") as num_groups
+from stud s 
+group by gender, "race/ethnicity"
+order by num_groups desc;
+
+
 -- # of students by "parental level of education" 
 select "parental level of education" , 
 count("parental level of education") as num_parental_lvl_edu
 from stud s 
 group by "parental level of education" 
+order by num_parental_lvl_edu desc;
+
+
+-- # of students by "parental level of education" grouped by gender
+select gender,
+"parental level of education" , 
+count("parental level of education") as num_parental_lvl_edu
+from stud s 
+group by gender, "parental level of education" 
 order by num_parental_lvl_edu desc;
 
 
@@ -51,6 +69,15 @@ group by lunch
 order by num_lunch desc;
 
 
+-- # of students by lunch grouped by gender
+select gender, 
+lunch, 
+count(lunch) as num_lunch
+from stud s 
+group by gender, lunch 
+order by num_lunch desc;
+
+
 -- # of students by "test preparation course" 
 select "test preparation course" , 
 count("test preparation course") as num_test_prep_course
@@ -59,16 +86,24 @@ group by "test preparation course"
 order by num_test_prep_course desc;
 
 
+-- # of students by "test preparation course" grouped by gender
+select gender, 
+"test preparation course" , 
+count("test preparation course") as num_test_prep_course
+from stud s 
+group by gender, "test preparation course"  
+order by num_test_prep_course desc;
+
+
 -- 2. What factors influence students performance in exam ?
-select "race/ethnicity", 
-"parental level of education", 
+select "parental level of education", 
 lunch, 
 "test preparation course", 
 avg("math score") as avg_math_score,
 avg("reading score") as avg_reading_score,
 avg("writing score") as avg_writing_score
 from stud
-group by "race/ethnicity", "parental level of education", lunch, "test preparation course" 
+group by "parental level of education", lunch, "test preparation course" 
 order by avg_math_score desc, avg_reading_score desc, avg_writing_score desc
 
 
@@ -80,7 +115,6 @@ avg("writing score") as avg_writing_score
 from stud s 
 group by gender 
 order by avg_math_score desc, avg_reading_score desc, avg_writing_score desc
-
 
 
 -- avg test scores by race/ethnicity
