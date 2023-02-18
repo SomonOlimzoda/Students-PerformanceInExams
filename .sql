8f92224279c3793e3fa2b8bab5b076ba9103dd7f
Somon <somon.olimzoda1@gmail.com>
2023-02-18 19:40:57 +0900

    Update .sql

--- .sql
+++ .sql
@@ -1,25 +1,21 @@
-1. In what ways male and female students differ?
-2. What factors influence students performance in exam ?
-	1. It is important that they complete the preparatory courses.
-	2. It is important for families to increase their education level and be open to learning.
-	3. It is important that they have a standart plan for food
-3. How to imporve the students performance in each test ?
-6. avg test scores of students by gender
-7. Distribution of scores by column for each test score
-8. Does the parent level of education has any impact in the student score ?
-9. Is the test preparation course helpful ?
-10. Does the luch affect the student scores ?
-11. is there any difference in scores between different ethnicities
-12. Does lunch affect exam scores?
+
+# Questions to be answered
+
+1. What factors influence students' performance in exams?
+2. What would be the best way to improve student scores on each test?
+3. How effective is the test preparation course?
+
+##
 
 
---------------------------------------------------------------------------------------------
+# let's check our dataset
 
 select * 
 from stud
 
 
--- # of students by gender 
+# of students by gender 
+
 select gender, 
 count(gender) as num_students
 from stud s 
@@ -27,7 +23,8 @@ group by gender
 order by  num_students desc;
 
 
--- # of students by race/ethnicity 
+# of students by race/ethnicity 
+
 select "race/ethnicity", 
 count("race/ethnicity") as num_groups
 from stud s 
@@ -35,7 +32,8 @@ group by "race/ethnicity"
 order by num_groups desc;
 
 
--- # of students by race/ethnicity grouped by gender
+# of students by race/ethnicity grouped by gender
+
 select gender,
 "race/ethnicity", 
 count("race/ethnicity") as num_groups
@@ -44,7 +42,8 @@ group by gender, "race/ethnicity"
 order by num_groups desc;
 
 
--- # of students by "parental level of education" 
+# of students by "parental level of education" 
+
 select "parental level of education" , 
 count("parental level of education") as num_parental_lvl_edu
 from stud s 
@@ -52,7 +51,8 @@ group by "parental level of education"
 order by num_parental_lvl_edu desc;
 
 
--- # of students by "parental level of education" grouped by gender
+# of students by "parental level of education" grouped by gender
+
 select gender,
 "parental level of education" , 
 count("parental level of education") as num_parental_lvl_edu
@@ -61,7 +61,8 @@ group by gender, "parental level of education"
 order by num_parental_lvl_edu desc;
 
 
--- # of students by lunch 
+# of students by lunch 
+
 select lunch, 
 count(lunch) as num_lunch
 from stud s 
@@ -69,7 +70,8 @@ group by lunch
 order by num_lunch desc;
 
 
--- # of students by lunch grouped by gender
+# of students by lunch grouped by gender
+
 select gender, 
 lunch, 
 count(lunch) as num_lunch
@@ -78,7 +80,8 @@ group by gender, lunch
 order by num_lunch desc;
 
 
--- # of students by "test preparation course" 
+# of students by "test preparation course" 
+
 select "test preparation course" , 
 count("test preparation course") as num_test_prep_course
 from stud s 
@@ -86,7 +89,8 @@ group by "test preparation course"
 order by num_test_prep_course desc;
 
 
--- # of students by "test preparation course" grouped by gender
+# of students by "test preparation course" grouped by gender
+
 select gender, 
 "test preparation course" , 
 count("test preparation course") as num_test_prep_course
@@ -95,7 +99,8 @@ group by gender, "test preparation course"
 order by num_test_prep_course desc;
 
 
--- 2. What factors influence students performance in exam ?
+
+
 select "parental level of education", 
 lunch, 
 "test preparation course", 
@@ -107,7 +112,8 @@ group by "parental level of education", lunch, "test preparation course"
 order by avg_math_score desc, avg_reading_score desc, avg_writing_score desc
 
 
--- avg test scores by gender
+avg test scores by gender
+
 select gender, 
 avg("math score") as avg_math_score,
 avg("reading score") as avg_reading_score,
@@ -117,7 +123,8 @@ group by gender
 order by avg_math_score desc, avg_reading_score desc, avg_writing_score desc
 
 
--- avg test scores by race/ethnicity
+avg test scores by race/ethnicity
+
 select "race/ethnicity",
 avg("math score") as avg_math_score,
 avg("reading score") as avg_reading_score,
@@ -127,7 +134,8 @@ group by "race/ethnicity"
 order by avg_math_score desc, avg_reading_score desc, avg_writing_score desc
 
 
--- avg test scores by race/ethnicity and gender
+avg test scores by race/ethnicity and gender
+
 select gender,
 "race/ethnicity",
 avg("math score") as avg_math_score,
@@ -138,7 +146,8 @@ group by gender, "race/ethnicity"
 order by avg_math_score desc, avg_reading_score desc, avg_writing_score desc
 
 
--- avg test scores by parental level of education
+avg test scores by parental level of education
+
 select "parental level of education",
 avg("math score") as avg_math_score,
 avg("reading score") as avg_math_score,
@@ -148,7 +157,8 @@ group by "parental level of education"
 order by avg_math_score desc, avg_math_score desc, avg_math_score desc
 
 
--- avg test scores by parental level of education and gender
+avg test scores by parental level of education and gender
+
 select gender,
 "parental level of education",
 avg("math score") as avg_math_score,
@@ -159,7 +169,8 @@ group by gender, "parental level of education"
 order by avg_math_score desc, avg_math_score desc, avg_math_score desc
 
 
--- avg test scores by test preparation course
+avg test scores by test preparation course
+
 select "test preparation course", 
 avg("math score") as avg_math_score,
 avg("reading score") as avg_math_score,
@@ -169,7 +180,8 @@ group by "test preparation course"
 order by avg_math_score desc, avg_math_score desc, avg_math_score desc
 
 
--- avg test scores by test preparation course and gender 
+avg test scores by test preparation course and gender 
+
 select gender,
 "test preparation course", 
 avg("math score") as avg_math_score,
@@ -180,7 +192,8 @@ group by gender, "test preparation course"
 order by avg_math_score desc, avg_math_score desc, avg_math_score desc
 
 
--- avg test scores by lunch
+avg test scores by lunch
+
 select lunch, 
 avg("math score") as avg_math_score,
 avg("reading score") as avg_math_score,
@@ -190,7 +203,8 @@ group by lunch
 order by avg_math_score desc, avg_math_score desc, avg_math_score desc
 
 
--- avg test scores by lunch and gender
+avg test scores by lunch and gender
+
 select gender,
 lunch, 
 avg("math score") as avg_math_score,
